a2010d279b4029e21ae0461a3100e3f8d3b6a875
lukejacksonn <lukejacksonn@gmail.com>
2024-04-14 21:09:57 +0100

    Implement useScrollProgressHook

---
+++ hooks/useScrollProgress.ts
@@ -0,0 +1,42 @@
+import { useRef, useState, useEffect } from "preact/hooks";
+
+export const useScrollProgress = () => {
+  // Ref for the scrollable element
+  const ref = useRef<HTMLElement>(null);
+  // State for progress
+  const [progress, setProgress] = useState({
+    percent: 0,
+    page: {
+      current: 1,
+      total: 1,
+    },
+  });
+  // Subscribe to events that trigger update of state
+  useEffect(() => {
+    const el = ref.current as HTMLElement;
+    if (el) {
+      const update = () => {
+        const progress = el.scrollLeft / (el.scrollWidth - el.clientWidth);
+        const pageCount = Math.round(el.scrollWidth / el.clientWidth);
+        setProgress({
+          percent: progress * 100,
+          page: {
+            current: Math.round(progress / (1 / pageCount)) || 1,
+            total: pageCount,
+          },
+        });
+      };
+      // Update progress on parent element resizing
+      const resizeObserver = new ResizeObserver(update);
+      resizeObserver.observe(el);
+      // Update progress on scroll
+      el.addEventListener("scroll", update);
+      return () => {
+        resizeObserver.disconnect();
+        el.removeEventListener("scroll", update);
+      };
+    }
+  }, [ref]);
+  // Return the ref and state
+  return { ref, ...progress };
+};
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,13 +1,18 @@
-import { useState } from "preact/hooks";
+import { useMemo, useState } from "preact/hooks";
 import { Footer } from "../components/Footer";
 import { Main } from "../components/Main";
 import { Center, Nav } from "../components/Nav";
 import { Screen } from "../components/Screen";
+import { useScrollProgress } from "../hooks/useScrollProgress";
 
 const AppIcon = (props: { name?: string; icon: string }) => {
   return (
     <div class="col aic gap-1 px-0.5">
-      <img src={`/images/${props.icon}`} alt={props.name} />
+      <img
+        src={`/images/${props.icon}`}
+        alt={props.name}
+        class="aspect-square w-full"
+      />
       {props.name && <p class="text-center text-xs">{props.name}</p>}
     </div>
   );
@@ -15,17 +20,27 @@ const AppIcon = (props: { name?: string; icon: string }) => {
 
 export const Home = () => {
   const [apps, setApps] = useState(installed);
-  const list = Object.entries(apps)
-    .map(([id, v]) => ({ id, ...v }))
-    .sort(() => (Math.random() > 0.5 ? 1 : -1));
-  let split = rand(5, list.length - 2);
+  const list = useMemo(
+    () =>
+      Object.entries(apps)
+        .map(([id, v]) => ({ id, ...v }))
+        .sort(() => (Math.random() > 0.5 ? 1 : -1)),
+    []
+  );
+
+  let split = useMemo(() => rand(5, list.length - 2), []);
+  const { ref, page } = useScrollProgress();
+
   return (
     <>
       <Screen>
         <img src="/images/image.jpeg" class="absolute inset-0 w-full h-full" />
         <Main class="pt-safe-t backdrop-blur-lg pb-footer">
           <div class="relative flex-1 col">
-            <div class="flex-1 row overflow-x-scroll snap-x snap-mandatory children:(snap-center snap-always)">
+            <div
+              ref={ref}
+              class="flex-1 row overflow-x-scroll snap-x snap-mandatory children:(snap-center snap-always)"
+            >
               <div class="w-full flex-none grid grid-cols-4 grid-rows-6 p-6 gap-x-6">
                 {list.slice(0, split).map((app) => (
                   <AppIcon {...app} />
@@ -37,9 +52,14 @@ export const Home = () => {
                 ))}
               </div>
             </div>
-            <div class="self-center row gap-2 py-2.5 px-4 rounded-full bg-black/20 children:(w-2 h-2 bg-white/50 rounded-full) mb-1">
-              <div class="!bg-white"></div>
-              <div></div>
+            <div class="self-center row gap-2 py-2.5 px-4 rounded-full bg-black/20 children:(w-2 h-2 rounded-full) mb-1">
+              {Array.from({ length: page.total }).map((_, i) => (
+                <div
+                  class={`w-2 h-2 rounded-full ${
+                    i + 1 === page.current ? "bg-white" : "bg-white/50"
+                  }`}
+                />
+              ))}
             </div>
           </div>
         </Main>
