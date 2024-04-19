1f40f17ba96e7a5d9e74bec038ce8feac74fa243
lukejacksonn <lukejacksonn@gmail.com>
2024-04-19 20:55:00 +0100

    Move Velocity function out of use Effect

--- screens/playground.tsx
+++ screens/playground.tsx
@@ -5,7 +5,7 @@ export default () => {
   useEffect(() => {
     const _C = document.querySelector(".lol"),
       N = _C.children.length,
-      NF = 10;
+      NF = 12;
 
     const TFN = {
       linear: function (k) {
@@ -14,7 +14,7 @@ export default () => {
       "ease-in": function (k, e = 1.675) {
         return Math.pow(k, e);
       },
-      "ease-out": function (k, e = 1.675) {
+      "ease-out": function (k, e = 1.618) {
         return 1 - Math.pow(1 - k, e);
       },
       "ease-in-out": function (k) {
@@ -115,41 +115,6 @@ export default () => {
 
     _C.addEventListener("mouseup", move, false);
     _C.addEventListener("touchend", move, false);
-
-    function Velocity() {
-      this.positionQueue = [];
-      this.timeQueue = [];
-    }
-
-    Velocity.prototype.reset = function () {
-      this.positionQueue.splice(0);
-      this.timeQueue.splice(0);
-    };
-
-    Velocity.prototype.pruneQueue = function (ms) {
-      //pull old values off of the queue
-      while (this.timeQueue.length && this.timeQueue[0] < Date.now() - ms) {
-        this.timeQueue.shift();
-        this.positionQueue.shift();
-      }
-    };
-
-    Velocity.prototype.updatePosition = function (position) {
-      this.positionQueue.push(position);
-      this.timeQueue.push(Date.now());
-      this.pruneQueue(50);
-    };
-
-    Velocity.prototype.getVelocity = function () {
-      this.pruneQueue(1000);
-      var length = this.timeQueue.length;
-      if (length < 2) return 0;
-
-      var distance = this.positionQueue[length - 1] - this.positionQueue[0],
-        time = (this.timeQueue[length - 1] - this.timeQueue[0]) / 1000;
-
-      return distance / time;
-    };
   }, []);
 
   return (
@@ -204,4 +169,39 @@ export default () => {
       <div class="screen">5</div>
     </div>
   );
+};
+
+function Velocity() {
+  this.positionQueue = [];
+  this.timeQueue = [];
+}
+
+Velocity.prototype.reset = function () {
+  this.positionQueue.splice(0);
+  this.timeQueue.splice(0);
+};
+
+Velocity.prototype.pruneQueue = function (ms) {
+  //pull old values off of the queue
+  while (this.timeQueue.length && this.timeQueue[0] < Date.now() - ms) {
+    this.timeQueue.shift();
+    this.positionQueue.shift();
+  }
+};
+
+Velocity.prototype.updatePosition = function (position) {
+  this.positionQueue.push(position);
+  this.timeQueue.push(Date.now());
+  this.pruneQueue(50);
+};
+
+Velocity.prototype.getVelocity = function () {
+  this.pruneQueue(1000);
+  var length = this.timeQueue.length;
+  if (length < 2) return 0;
+
+  var distance = this.positionQueue[length - 1] - this.positionQueue[0],
+    time = (this.timeQueue[length - 1] - this.timeQueue[0]) / 1000;
+
+  return distance / time;
 };
