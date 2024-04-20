87bc9fa67c0f6eed79d8614f92eeb0cd14cf26dc
lukejacksonn <lukejacksonn@gmail.com>
2024-04-20 20:12:22 +0100

    Stuck screen and scroll lock

--- screens/playground.tsx
+++ screens/playground.tsx
@@ -9,13 +9,14 @@ export default () => {
     const NF = 12;
 
     const TFN = {
-      "ease-out": (k: number, e = 1.675) => {
+      "ease-out": (k: number, e = 1.618) => {
         return 1 - Math.pow(1 - k, e);
       },
     };
 
     let i = parseInt($.style.getPropertyValue("--i")) || 0;
     let x0: number = 0;
+    let xY: number = 0;
     let locked = false;
     let rID: number | null = null;
     let w = window.innerWidth;
@@ -23,9 +24,15 @@ export default () => {
     let fin: number;
     let anf: number | undefined;
     let progress: number = 0;
+    let isDragging = false;
+    let totalDistance = 0;
+    let scroller = document.querySelector(
+      "[data-scrolling]"
+    ) as HTMLElement | null;
 
     // @ts-ignore
     let v = Velocity();
+    let yV = Velocity();
 
     function stopAni() {
       rID && cancelAnimationFrame(rID);
@@ -48,34 +55,55 @@ export default () => {
     }
 
     function lock(e: MouseEvent | TouchEvent) {
+      if (rID) isDragging = true;
       stopAni();
       x0 = unify(e).clientX;
+      xY = unify(e).clientY;
+      v.reset();
+      yV.reset();
       locked = true;
+      totalDistance = 0;
+      scroller = document.querySelector("[data-scrolling]");
     }
 
     function drag(e: MouseEvent | TouchEvent) {
-      e.preventDefault();
-      if (locked) {
-        const dx = unify(e).clientX - x0;
-        const f = +(dx / w);
-        v.updatePosition(unify(e).clientX);
+      const dx = unify(e).clientX - x0;
+      const dy = unify(e).clientY - xY;
+      const f = +(dx / w);
+      v.updatePosition(unify(e).clientX);
+      yV.updatePosition(unify(e).clientY);
+      totalDistance += Math.abs(dx) + Math.abs(dy);
+      if (
+        (totalDistance < 300 &&
+          Math.abs(yV.getVelocity()) < 1000 &&
+          Math.abs(v.getVelocity()) > 300 &&
+          Math.abs(90 - Math.abs((Math.atan2(dx, dy) * 180) / Math.PI)) < 25) ||
+        isDragging
+      ) {
+        scroller && (scroller.style.overflowY = "hidden");
+        isDragging = true;
         const next = (progress || i) - f;
         $.style.setProperty("--i", "" + next);
       }
     }
 
     function move(e: MouseEvent | TouchEvent) {
-      if (locked) {
-        let dx = unify(e).clientX - x0,
-          s = Math.sign(dx),
-          f = +((s * dx) / w);
+      const dx = unify(e).clientX - x0;
+      const dy = unify(e).clientY - xY;
+      if (locked && isDragging) {
+        const s = Math.sign(dx);
+        let f = +((s * dx) / w);
 
         ini = (progress || i) - s * f;
 
         if (
           (i > 0 || s < 0) &&
           (i < N - 1 || s > 0) &&
-          (f > 0.5 || Math.abs(v.getVelocity()) > 500)
+          (f > 0.5 ||
+            (Math.abs(yV.getVelocity()) < 1000 &&
+              Math.abs(v.getVelocity()) > 300 &&
+              Math.abs(90 - Math.abs((Math.atan2(dx, dy) * 180) / Math.PI)) <
+                25))
         ) {
           i -= s;
           f = 1 - f;
@@ -85,7 +113,10 @@ export default () => {
         anf = Math.round(f * NF);
         ani();
         x0 = 0;
+        xY = 0;
         locked = false;
+        isDragging = false;
+        scroller && (scroller.style.overflowY = "scroll");
       }
     }
 
@@ -100,54 +131,103 @@ export default () => {
   }, []);
 
   return (
-    <div
-      ref={ref}
-      style="--i: 4; --n: 5;"
-      class={cx(
-        css`
-          display: flex;
-          align-items: center;
-          overflow-y: hidden;
-          width: calc(var(--n) * 100%);
-          height: 100vh;
-          max-height: 100vh;
-          transform: translate(calc(var(--i, 0) / var(--n) * -100%));
-
-          & .screen {
-            flex: none;
+    <>
+      <div
+        ref={ref}
+        style="--i: 1; --n: 5;"
+        class={cx(
+          css`
             display: flex;
             align-items: center;
-            justify-content: center;
-            height: 100%;
-            width: 100vw;
-            color: white;
-            font-family: sans-serif;
-            font-size: 50vmin;
-            &:nth-child(1) {
-              background: cyan;
+            overflow-y: hidden;
+            width: calc(var(--n) * 100%);
+            height: 100vh;
+            max-height: 100vh;
+            transform: translate(calc(var(--i, 0) / var(--n) * -100%));
+
+            & .screen {
+              position: relative;
+              z-index: 99;
+              flex: none;
+              display: flex;
+
+              height: 100%;
+              width: 100vw;
+              color: white;
+              font-family: sans-serif;
+              font-size: 5vmin;
+              &:nth-child(1) {
+                background: cyan;
+              }
+              &:nth-child(2) {
+                background: green;
+              }
+              &:nth-child(3) {
+                background: blue;
+              }
+              &:nth-child(4) {
+                background: purple;
+              }
+              &:nth-child(5) {
+                background: pink;
+              }
             }
-            &:nth-child(2) {
-              background: green;
-            }
-            &:nth-child(3) {
-              background: blue;
-            }
-            &:nth-child(4) {
-              background: purple;
-            }
-            &:nth-child(5) {
-              background: pink;
-            }
-          }
-        `
-      )}
-    >
-      <div class="screen">1</div>
-      <div class="screen">2</div>
-      <div class="screen">3</div>
-      <div class="screen">4</div>
-      <div class="screen">5</div>
-    </div>
+          `
+        )}
+      >
+        <div class="screen">0</div>
+        <div
+          class="screen"
+          style={`position: relative; z-index: 0; transform: translateX(calc(calc(-100% + calc(min(2, var(--i, 0)) * 100%))`}
+        >
+          <VerticalScroll />
+        </div>
+        <div class="screen">2</div>
+        <div class="screen">3</div>
+        <div class="screen">4</div>
+      </div>
+      <div id="output_log" class="fixed top-16 left-16"></div>
+    </>
+  );
+};
+
+const VerticalScroll = () => {
+  // useEffect(() => {
+  //   const scroller = document.querySelector("[data-scrolling]");
+  //   if (scroller) {
+  //     // scroller.addEventListener("touchstart", (e) => {
+  //     //   e.preventDefault();
+  //     // });
+  //     scroller.addEventListener("scroll", (e) => {
+  //       console.log(e.target.scrollTop);
+  //     });
+  //   }
+  // }, []);
+
+  return (
+    <p class="overflow-y-scroll pt-safe-t p-4 pb-safe-b" data-scrolling="false">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime adipisci
+      sapiente culpa doloremque rerum voluptates laboriosam asperiores error
+      aliquam? Consequatur reiciendis quam aliquam totam eaque qui suscipit
+      aliquid minima, distinctio in odio harum expedita sunt rerum natus.
+      Delectus odio earum ipsa, voluptatibus distinctio ut esse voluptas
+      recusandae voluptate ipsum iure provident labore, reprehenderit atque
+      dolorem beatae vitae quod officia vero reiciendis vel inventore facilis
+      impedit? Beatae ipsum sunt architecto fugiat praesentium aliquid possimus,
+      earum ipsa exercitationem quisquam! Culpa tempora cumque quidem eum, fuga
+      minus error nulla beatae voluptatem, quis officiis aspernatur omnis
+      officia nostrum, consequuntur veniam autem commodi rem? Eos dicta omnis,
+      nisi aut dolorum tempore sapiente eligendi eum tempora provident libero
+      fugit culpa esse illo, quae quas laborum repellendus. Perferendis
+      laudantium mollitia non cum architecto quisquam laborum? Sint quisquam ut
+      iusto, pariatur labore in porro asperiores odio dolore ex doloremque quae.
+      Pariatur est obcaecati cupiditate aspernatur neque ea nihil possimus iste
+      quibusdam dolores. Animi illo at minus? Repudiandae, doloremque unde
+      labore voluptatum temporibus fugiat, et sapiente officiis omnis, quidem
+      quasi ad laudantium reprehenderit! Culpa veniam iusto quae, itaque facere
+      beatae unde dolorem quo eveniet animi dolorum expedita? Natus consequatur
+      ea, veniam fugiat at nobis recusandae ipsum consequuntur culpa quis.
+    </p>
   );
 };
 
