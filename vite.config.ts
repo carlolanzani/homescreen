e546d4ad97b0a500a4b18fd4049012e64c4a74c9
lukejacksonn <lukejacksonn@gmail.com>
2024-10-01 14:47:39 +0100

    Change output dir to dist

--- vite.config.ts
+++ vite.config.ts
@@ -7,41 +7,41 @@ export default defineConfig({
     port: 5174,
   },
   build: {
-    outDir: "docs",
+    outDir: "dist",
   },
 });
 
 // @ts-ignore
-import fs from "fs";
-import chokidar from "chokidar";
+// import fs from "fs";
+// import chokidar from "chokidar";
 
-const directoryToWatch = "public/icons";
+// const directoryToWatch = "public/icons";
 
-const watcher = chokidar.watch(directoryToWatch, {
-  ignored: /(^|[\/\\])\../, // ignore dotfiles
-  persistent: true,
-});
+// const watcher = chokidar.watch(directoryToWatch, {
+//   ignored: /(^|[\/\\])\../, // ignore dotfiles
+//   persistent: true,
+// });
 
-watcher.on("add", (filePath) => {
-  fs.readFile(filePath, "utf8", (err: Error, data: string) => {
-    if (err) {
-      console.error(`Error reading file: ${err}`);
-      return;
-    }
+// watcher.on("add", (filePath) => {
+//   fs.readFile(filePath, "utf8", (err: Error, data: string) => {
+//     if (err) {
+//       console.error(`Error reading file: ${err}`);
+//       return;
+//     }
 
-    const modifiedContent = data
-      .replace(/<svg width="28" height="28" /, '<svg id="icon" ')
-      .replace(/fill="#1C1C1E"/g, 'fill="currentColor"');
+//     const modifiedContent = data
+//       .replace(/<svg width="28" height="28" /, '<svg id="icon" ')
+//       .replace(/fill="#1C1C1E"/g, 'fill="currentColor"');
 
-    fs.writeFile(filePath, modifiedContent, "utf8", (err: Error) => {
-      if (err) {
-        console.error(`Error writing file: ${err}`);
-        return;
-      }
+//     fs.writeFile(filePath, modifiedContent, "utf8", (err: Error) => {
+//       if (err) {
+//         console.error(`Error writing file: ${err}`);
+//         return;
+//       }
 
-      console.log(`File ${filePath} has been modified.`);
-    });
-  });
-});
+//       console.log(`File ${filePath} has been modified.`);
+//     });
+//   });
+// });
 
-console.log(`Watching directory: ${directoryToWatch}`);
+// console.log(`Watching directory: ${directoryToWatch}`);
